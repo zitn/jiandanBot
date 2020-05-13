a707fd9c73170200153be770ce74e8ffcaf0c838
zigitn <me@5a6c.me>
2020-05-13 12:35:48 +0800

    修改配置文件位置, 使得docker与直接运行两种方式的config文件位置兼容

--- main.go
+++ main.go
@@ -12,9 +12,9 @@ import (
 )
 
 func init() {
-	viper.SetConfigName("config")  //配置文件名
-	viper.AddConfigPath("/config") //配置文件所在的路径
-	viper.SetConfigType("json")    //配置文件类型
+	viper.SetConfigName("config") //配置文件名
+	viper.AddConfigPath("config") //配置文件所在的路径
+	viper.SetConfigType("json")   //配置文件类型
 	err := viper.ReadInConfig()
 	if err != nil {
 		logrus.Panic(err)
